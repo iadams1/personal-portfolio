cf3c38c62e22627c3a48ef1eb56edb478fb30d05
Isaiah Adams <IJAdams1@outlook.com>
2025-08-28 09:53:10 -0500

    Created the pages for Projects, Latest and More Projects page, where projects is going to import the HTML from said pages.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,14 @@
+import { LatestProjects } from "./project-components/LatestProjects"
+import { MoreProjects } from "./project-components/MoreProjects";
+
 export const Projects = () => {
-    return <></>;
+    return (
+        <>
+            {/* Projects Pages */}
+            <LatestProjects />
+            <MoreProjects/>
+        </>
+    );
 };
+
+export default Projects
---
+++ src/components/project-components/MoreProjects.jsx
@@ -0,0 +1,4 @@
+
+export const MoreProjects = () => {
+    return <></>;
+};
