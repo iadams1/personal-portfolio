25824464ff26233b4a629162cea3e13982aafe4f
Isaiah Adams <IJAdams1@outlook.com>
2025-08-30 01:11:49 -0500

    Added resume to the site as well as the animations to the contacts page. Added and edited other CSS styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ export const Contact = () => {
                 <div className="contact-content">
                     <div className="contact-grid">
                         <div className="contact-grid-information">
-                            <h1 className="contact-information-title">Contact</h1>
+                            <h1 className="contact-information-title">Contact Me</h1>
                             <p className="contact-information-text">
                                 I'd love to connect! Whether you have questions about my work, 
                                 opportunities to collaborate, or just want to chat about technology, feel free to reach out. You can 
@@ -21,13 +21,45 @@ export const Contact = () => {
                             <p className="contact-information-subtext">
                                 Located in Little Rock, Arkansas — open to in-person opportunities and connections.
                             </p>
-                            <div className="contact-information-button">
-                                <img src="/Paperclip.png" className="contact-information-paperclip"/>
-                                <h3 className="contact-information-button-text">Get Resume</h3>
-                            </div>
+                            <a
+                                href="/IsaiahAdams_Resume.pdf" 
+                                download="IsaiahAdams_Resume.pdf"
+                                style={{ textDecoration: "none", color: "inherit", alignSelf: "center"}}
+                            >
+                                <motion.div 
+                                className="contact-information-button"
+                                whileHover={{ scale: 1.05 }}
+                                whileTap={{ scale: 0.95 }}
+                                >
+                                    <img src="/Paperclip.png" className="contact-information-paperclip"/>
+                                    <h3 className="contact-information-button-text">Get Resume</h3>
+                                </motion.div>
+                            </a>
+                            
                             <div className="contact-information-icons">
-                                <img src="/_Github.png"/>
-                                <img src="/_Linkedin.png"/>
+                                <a 
+                                    href="https://github.com/iadams1"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                  <motion.img
+                                    whileHover={{ scale: 1.15 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    src="/_Github.png"
+                                    />  
+                                </a>
+                                <a 
+                                    href="https://www.linkedin.com/in/isaiah-j-adams/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <motion.img
+                                        whileHover={{ scale: 1.10 }}
+                                        whileTap={{ scale: 0.95 }}
+                                        src="/_Linkedin.png"
+                                    />
+                                </a>
+                                
                             </div>
                         </div>
                         <div className="contact-grid-form">
@@ -49,10 +81,16 @@ export const Contact = () => {
                                     <h3 className="contact-form-text">Message</h3>
                                     <textarea name="message" placeholder="Type your message here..." className="form-message" required/>
                                 </label>
-                                <button className="form-button">
-                                    <img src="/Send.png" className="form-submit-icon"/>
+                                <motion.button 
+                                    className="form-button"
+                                    whileHover={{ scale: 1.10 }}
+                                    whileTap={{ scale: 0.95 }}
+                                >
+                                    <div className="form-submit-icon">
+                                        <img src="/Send.png"/>   
+                                    </div>
                                     <span className="form-submit-text">Send</span>
-                                </button>
+                                </motion.button>
                             </form>
                         </div>
                     </div>
